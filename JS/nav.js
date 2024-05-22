93e9a44017e31ade4ba3e519225245f8e4f35954
BiraBalaZ <erick.m.anjos@gmail.com>
2024-05-21 21:35:42 -0300

    Organizing files

---
+++ JS/nav.js
@@ -0,0 +1,1 @@
+document.write(`<ul><li><a href="../HTML/index.html">Home</a></li><li><a href="../HTML/contato.html">Contato</a></li><li><a href="../HTML/sobre.html">Sobre</a></li></ul>`)
--- nav.js
+++
@@ -1,1 +0,0 @@
-document.write(`<ul><li><a href="index.html">Home</a></li><li><a href="contato.html">Contato</a></li><li><a href="sobre.html">Sobre</a></li></ul>`)
